489047833c207aed7952a2903488d6e7e8421867
Alex Mikhalev <alex@sci-blog.com>
2022-06-15 01:58:19 +0100

    Hack around the bug

--- start_docker.sh
+++ start_docker.sh
@@ -1,4 +1,4 @@
 gears-cli run --host rgcluster --port 30001 tokenizer_gears_for_sum.py --requirements requirements.txt
 pip3 install -r requirements.txt 
 pip3 install torch==1.9.1+cpu -f https://download.pytorch.org/whl/torch_stable.html
-python3.7 summary_processor_t5.py
+python3 summary_processor_t5.py
